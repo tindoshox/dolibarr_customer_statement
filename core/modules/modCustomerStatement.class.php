b1a9fa7e6388f1a667aa42046f8bbe8820b725fc
Tendai Shoko <tindoshox@gmail.com>
2026-08-11 15:58:51 +0200

    Change version number

--- core/modules/modCustomerStatement.class.php
+++ core/modules/modCustomerStatement.class.php
@@ -76,7 +76,7 @@ class modCustomerStatement extends DolibarrModules
         $this->editor_squarred_logo = '';					// Must be image filename into the module/img directory followed with @modulename. Example: 'myimage.png@customerstatement'
 
         // Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-        $this->version = '1.0.2';
+        $this->version = '1.0.3';
         // Url to the file with your last numberversion of this module
         //$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
